2dae94a293258798a2e0834e405b35f021aa5e67
Colin <wfang@nd.edu>
2021-06-26 12:57:51 +0800

    fizzbuzz skeleton

---
+++ fizzbuzz.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import os
+
+# Functions
+
+def fizzbuzz(start=1, stop=100):
+    ''' 
+    >>> fizzbuzz(1, 5)
+    1
+    2
+    Fizz
+    4
+    Buzz
+    
+    >>> fizzbuzz(12, 15)
+    Fizz
+    13
+    14
+    FizzBuzz
+    '''
+    # START CODE HERE
+
+
+
+# Main Execution
+
+if __name__ == '__main__':
+    fizzbuzz()
